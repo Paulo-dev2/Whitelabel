f3ad1209bff9d78243af2116fedf73f3635f3b55
Paulo Abdiel <abdielpaulo2@gmail.com>
2025-11-11 18:20:41 -0300

    fix(client): Use Host header instead of query parameter for Whitelabel identification

--- app/backend/src/client/client.controller.ts
+++ app/backend/src/client/client.controller.ts
@@ -1,15 +1,17 @@
-import { Controller, Get, Query, HttpException, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Headers, HttpException, HttpStatus } from '@nestjs/common';
 import { ClientService } from './client.service';
 import { Client } from '@prisma/client';
 
 @Controller('clients')
 export class ClientController {
   constructor(private readonly clientService: ClientService) {}
+
   @Get('config')
-  async getClientConfig(@Query('host') hostUrl: string): Promise<Client> {
-    if (!hostUrl) {
-      throw new HttpException('Host URL query parameter is required.', HttpStatus.BAD_REQUEST);
+  async getClientConfig(@Headers('host') hostHeader: string): Promise<Client> {
+    if (!hostHeader) {
+      throw new HttpException('Host header is missing.', HttpStatus.BAD_REQUEST);
     }
+    const hostUrl = hostHeader.split(':')[0];
     return this.clientService.getClientConfigByHost(hostUrl);
   }
 }
