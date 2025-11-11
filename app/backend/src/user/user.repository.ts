744bf64b20c721b620e43d325c96a78dcd2c17a2
Paulo Abdiel <abdielpaulo2@gmail.com>
2025-11-11 17:48:15 -0300

    fix(userRepository): implements findByEmailAndClientId

--- app/backend/src/user/user.repository.ts
+++ app/backend/src/user/user.repository.ts
@@ -6,5 +6,18 @@ import { User } from '@prisma/client';
 export class UserRepository {
   constructor(private readonly prisma: PrismaService) {}
 
-
+  /**
+   * Busca um usuário pelo email E pela ID do cliente.
+   * Isso garante que um usuário da Loja Alpha não possa logar na Loja Beta.
+   */
+  async findByEmailAndClientId(email: string, clientId: number): Promise<User> {
+    const user = await this.prisma.user.findFirst({
+      where: { email, clientId },
+      include: { client: true },
+    });
+    if (!user) {
+      throw new NotFoundException('Invalid credentials.');
+    }
+    return user;
+  }
 }
