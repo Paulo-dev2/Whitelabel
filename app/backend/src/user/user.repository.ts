bfa43ed84d81bd893e25bace75ef85d922541893
Paulo Abdiel <abdielpaulo2@gmail.com>
2025-11-11 17:45:54 -0300

    feat(auth): Configure JWT integration in AuthModule and export UserService from UserModule

---
+++ app/backend/src/user/user.repository.ts
@@ -0,0 +1,10 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { User } from '@prisma/client';
+
+@Injectable()
+export class UserRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+
+}
