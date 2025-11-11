2b9bb8c152c77ed3feac4ca72f7988cc632d8df3
Paulo Abdiel <abdielpaulo2@gmail.com>
2025-11-11 17:40:22 -0300

    feat(client): Implement client identification endpoint for Whitelabel

--- app/backend/src/client/client.controller.ts
+++ app/backend/src/client/client.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, HttpException, HttpStatus } from '@nestjs/common';
+import { ClientService } from './client.service';
+import { Client } from '@prisma/client';
 
-@Controller('client')
-export class ClientController {}
+@Controller('clients')
+export class ClientController {
+  constructor(private readonly clientService: ClientService) {}
+  @Get('config')
+  async getClientConfig(@Query('host') hostUrl: string): Promise<Client> {
+    if (!hostUrl) {
+      throw new HttpException('Host URL query parameter is required.', HttpStatus.BAD_REQUEST);
+    }
+    return this.clientService.getClientConfigByHost(hostUrl);
+  }
+}
--- app/backend/src/client/client.service.ts
+++ app/backend/src/client/client.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ClientRepository } from './client.repository';
+import { Client } from '@prisma/client';
 
 @Injectable()
-export class ClientService {}
+export class ClientService {
+    constructor(private readonly clientRepository: ClientRepository) {}
+
+    async getClientConfigByHost(hostUrl: string): Promise<Client> {
+        const client = await this.clientRepository.findByHostUrl(hostUrl);
+        
+        return client;
+    }
+}
